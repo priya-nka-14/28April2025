67ed83034092fa640cc853c75e1a7c8ab61b48d2
Admin <Admin@DESKTOP-BP3UJSF>
2025-12-15 16:20:14 +0530

    Enter email and close browser

--- TestApril2025Project/src/test/java/javaPackage/Test1.java
+++ TestApril2025Project/src/test/java/javaPackage/Test1.java
@@ -1,5 +1,6 @@
 package javaPackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -15,7 +16,8 @@ public class Test1 {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
-		
+		driver.findElement(By.id("email")).sendKeys("abc@gmail.com");
+		driver.close();
 		
 	}
 
